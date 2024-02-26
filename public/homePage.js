14adad9e6f4764d9045d020efbd97156b09138c1
Maksim Chalyh <nazgulius@yandex.ru>
2024-02-26 23:58:06 +0300

    login page OK, home page update

--- public/homePage.js
+++ public/homePage.js
@@ -1,29 +1,51 @@
 const logoutButton = new LogoutButton ();
 
-logoutButton.action(
-    ApiConnector.logout(item => {
-        if (item.success){
-            location.reload();
-        }    
-    })
-); 
 
-ApiConnector.current(callback => {
+logoutButton.action = () => {
+     ApiConnector.logout(item => {        
+         if (item.success){
+             location.reload();
+         }    
+     })
+}; 
+
+ApiConnector.current(callback => {  
     if (callback.success) {
-        ProfileWidget.showProfile(callback);
+        ProfileWidget.showProfile(callback.data);
     }
 });
 
-const ratesBoard = new RatesBoard(data => {    
-    if (data) {
-        clearTable();
-        fillTable(data);
-    }
-});
+const ratesBoard = new RatesBoard();
 
-setInterval(() => {
-    ratesBoard();
-}, 1000 * 60);
+// setInterval(() => {     
+//      ratesBoard.tableBody = () => {
+//         console.log("TEST");
+//         ApiConnector.getStocks(data => {
+//             console.log(data);
+//             if (data.success) {
+//                 clearTable();
+//                 fillTable(data.date);
+//             }
+//         });    
+//     };
+// }, 1000);
+
+function func() {
+    ApiConnector.getStocks(item => {
+        console.log(item);
+        console.log(item.date);
+        if (item.success) {
+            console.log('внутри IF');
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(item);
+        }
+    }); 
+}
+
+func();
+setInterval(() => {     
+    func();
+}, 1000 *60);
 
 
 const moneyManager = new MoneyManager ();
